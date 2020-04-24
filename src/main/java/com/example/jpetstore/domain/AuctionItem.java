b085a34e0cb6ff8691fbd2f4a17cf9e45311f3d2
hjooooo <m2tkfkd@naver.com>
2020-04-24 12:43:42 +0900

    AuctionItem

---
+++ src/main/java/com/example/jpetstore/domain/AuctionItem.java
@@ -0,0 +1,49 @@
+package com.example.jpetstore.domain;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class AuctionItem implements Serializable{
+	/* Private Fields */
+	private String itemId;
+	private String auctionid;
+	private Date deadline;
+	private int deposit;
+	private Item item;
+	  
+	/* JavaBeans Properties */
+	public String getItemId() {
+		return itemId;
+	}
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+	public String getAuctionid() {
+		return auctionid;
+	}
+	public void setAuctionid(String auctionid) {
+		this.auctionid = auctionid;
+	}
+	public Date getDeadline() {
+		return deadline;
+	}
+	public void setDeadline(Date deadline) {
+		this.deadline = deadline;
+	}
+	public int getDeposit() {
+		return deposit;
+	}
+	public void setDeposit(int deposit) {
+		this.deposit = deposit;
+	}
+	public Item getItem() {
+		return item;
+	}
+	public void setItem(Item item) {
+		this.item = item;
+	}
+	
+	  
+	  
+
+}
